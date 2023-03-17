3e5c0d9c6b42ea3188b61c56873545b3acc1b888
Ethan <ez4698@gmail.com>
2023-03-17 15:58:27 -0600

    initial commit

---
+++ about.js
@@ -0,0 +1,31 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form submit');
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+let batDuckMan = document.querySelector('#duckbat')
+	const batduck = () => {
+		alert('You are BatDuck!')
+	}
+	batDuckMan.addEventListener('mouseover', batduck)
+
+
+	letmyForm=document.querySelector('form')
+
+	const newThing = (event) => {
+		event.preventDefault()
+
+		alert("That's awesome!")
+	}
+
+	myForm.addEventListener('submit', newThing)
